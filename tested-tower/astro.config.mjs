50866172a05a9d2decb0f7edfb2a1aa2cf837494
DeciusWoodson-FS <wsdecius@student.fullsail.edu>
2025-11-28 20:30:00 -0500

    Changed back to StaticSite

--- tested-tower/astro.config.mjs
+++ tested-tower/astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://deciuswoodson-FS.github.io",
-  base: "/tested-tower",
+  site: "https://DeciusWoodson-FS.github.io",
+  base: "/StaticSite",
 });
