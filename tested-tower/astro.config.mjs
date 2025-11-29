f54e25d48b6eb3862fce21e414dce2c229d7e576
DeciusWoodson-FS <wsdecius@student.fullsail.edu>
2025-11-28 19:23:03 -0500

    updated yml and config file

--- tested-tower/astro.config.mjs
+++ tested-tower/astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://DeciusWoodson-FS.github.io",
+  site: "https://deciuswoodson-FS.github.io",
   base: "/StaticSite",
 });
